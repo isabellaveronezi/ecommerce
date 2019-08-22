e0201021aa5f262d94c56b9d3c33dfac4a0cd252
Isabella Veronezi <isabella.veronezi@webjump.com.br>
2019-08-21 22:59:47 -0300

    Site Cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -355,13 +355,23 @@ class User extends Model {
         $_SESSION[User::ERROR_REGISTER] = NULL;
     }
 
+    public static function checkLoginExist($login)
+    {
+        $sql = new Sql(); 
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+            'deslogin'=>$login
+        ]);
+
+        return (count($results) > 0);
+    }
+
     public static function getPasswordHash($password)
     {
         return password_hash($password, PASSWORD_DEFAULT, [
             'cost'=>12
         ]);
     }
-
     
 }
    
