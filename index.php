a55a2c29fddd37ae9fea75f462cc43bc1e818d12
Isabella Veronezi <isabella.veronezi@webjump.com.br>
2019-08-23 14:23:10 -0300

    Page

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php
-	
-echo "hello world"
+<?php 
 
-?>
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+   
+	$page = new Hcode\Page();
+    
+    $page->setTpl("index");
+    var_dump($page);
+    exit;
+   
+
+});
+
+$app->run();
+
+ ?>
